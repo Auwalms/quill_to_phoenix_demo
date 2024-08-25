6897892316ceb47597149a30cbdd16c297fc81ad
Auwal MS <auwalforall@gmail.com>
2024-08-25 14:38:31 +0100

    5: customize the toolbar and renders html in raw format

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -11,13 +11,26 @@ Hooks.QuillEditor = {
 		this.initializeQuill();
 	},
 	initializeQuill() {
+		const toolbarOptions = [
+			[ { 'size': [] }],
+			[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+			['bold', 'italic', 'underline', 'strike'],
+			[{ 'color': [] }, { 'background': [] }],
+			[{ 'script': 'sub' }, { 'script': 'super' }],
+			[{ 'list': 'ordered' }, { 'list': 'bullet' }],
+			[{ 'indent': '-1' }, { 'indent': '+1' }],
+			[{ 'direction': 'rtl' }],
+			[{ 'align': [] }],
+			['link', 'image', 'video'],
+			['clean']
+		];
 
 
 		this.quill = new Quill('#editor-container', {
 			theme: 'snow',
 			placeholder: 'Type your content here...',
 			modules: {
-				//customization happens here
+				toolbar: toolbarOptions
 			}
 		});
 
